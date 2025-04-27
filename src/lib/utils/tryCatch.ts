e5f5a4b3566190630e65e533e45f924e9d60cbb8
Nils Röhrig <nils@desktop.fritz.box>
2025-04-27 21:21:12 +0200

    add async try catch util

---
+++ src/lib/utils/tryCatch.ts
@@ -0,0 +1,15 @@
+type Failure = [Error, null];
+type Success<TValue> = [null, TValue];
+export type Result<TValue> = Failure | Success<TValue>
+
+export async function tryCatchAsync<TValue>(fn: () => Promise<TValue>): Promise<Result<TValue>> {
+  try {
+    return [null, await fn()];
+  } catch (cause) {
+    if (cause instanceof Error) {
+      return [cause, null];
+    }
+
+    return [new Error(String(cause), {cause}), null];
+  }
+}

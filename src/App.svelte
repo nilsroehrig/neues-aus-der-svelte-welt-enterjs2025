79ec7c79405c6529d08fc0a1ef35a4d11a91edff
Nils Röhrig <nils@desktop.fritz.box>
2025-04-27 20:52:28 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<main>
+  <div class="game">
+    <article>
+      <header>Sveltenstein</header>
+      <ul>
+        <li>
+          <button>New Game</button>
+        </li>
+        <li>
+          <button>Highscore</button>
+        </li>
+      </ul>
+    </article>
+  </div>
+</main>
+
+<style>
+    main {
+        width: 100%;
+        height: 100vh;
+        overflow: auto;
+        display: grid;
+        align-content: center;
+    }
+
+    .game {
+        width: 20rem;
+        margin: auto;
+    }
+
+    header {
+        text-align: center;
+        font-weight: bold;
+    }
+
+    ul {
+        text-align: center;
+        padding: 0;
+        margin-bottom: 0;
+    }
+
+    li {
+        list-style: none;
+    }
+</style>

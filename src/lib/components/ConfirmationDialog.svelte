eef1b84ccf4a7f8fe33802ef67d13774ff434663
Nils Röhrig <nils@desktop.fritz.box>
2025-05-04 10:37:19 +0200

    add confirmation dialog to showcase snippets

---
+++ src/lib/components/ConfirmationDialog.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  let { header, children, onCancel, onConfirm, open = $bindable(false) }: {
+    header?: Snippet,
+    children: Snippet,
+    onConfirm: () => void,
+    onCancel: () => void,
+    open: boolean,
+  } = $props();
+</script>
+
+<dialog {open}>
+  <article>
+    {#if header}
+      <header>
+        {@render header()}
+        <button type="button" class="unstyled" onclick={onCancel}>&cross;</button>
+      </header>
+    {/if}
+    {@render children()}
+    <footer>
+      <button type="button" class="secondary" onclick={onCancel}>
+        Cancel
+      </button>
+
+      <button type="button" onclick={onConfirm}>
+        Confirm
+      </button>
+    </footer>
+  </article>
+</dialog>
+
+<style>
+  footer button {
+    margin-bottom: 0;
+  }
+</style>
--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -2,11 +2,14 @@
   import { type QuizDataSource } from "../data-sources/quiz.svelte.js";
   import Question from "./Question.svelte";
   import { wait } from "../utils/timers";
+  import ConfirmationDialog from "./ConfirmationDialog.svelte";
 
   const { quiz, onFinish }: { quiz: QuizDataSource, onFinish: () => void } = $props();
 
   let isLoadingQuestions = $derived(quiz.status === "pending");
   let isGameFinished = $derived(quiz.status === "finished");
+
+  let showConfirmation = $state(false);
 
   $effect(() => {
     quiz.init();
@@ -19,22 +22,38 @@
   }
 </script>
 
-{#if isLoadingQuestions}
-  <p aria-busy="true">Loading questions...</p>
-{:else if isGameFinished}
-  <p>Game over! Your score is {quiz.score}.</p>
-  <p>
-    <button class="secondary" onclick={onFinish}>Back to Menu</button>
-  </p>
-{:else}
-  <Question
-    question={quiz.currentQuestion!}
-    onAnswered={onAnswered}
-  />
-{/if}
+<div class="game">
+  {#if isLoadingQuestions}
+    <p aria-busy="true">Loading questions...</p>
+  {:else if isGameFinished}
+    <p>Game over! Your score is {quiz.score}.</p>
+    <p>
+      <button class="secondary" onclick={onFinish}>Back to Menu</button>
+    </p>
+  {:else}
+    <Question
+      question={quiz.currentQuestion!}
+      onAnswered={onAnswered}
+    />
+    <hr>
+    <p><button type="button" class="secondary outline" onclick={() => showConfirmation = true}>Cancel Game</button></p>
+  {/if}
+</div>
+
+<ConfirmationDialog
+  open={showConfirmation}
+  onConfirm={onFinish}
+  onCancel={() => showConfirmation = false}
+>
+  Do you really want to cancel the game?
+</ConfirmationDialog>
 
 <style>
   p {
     text-align: center;
+  }
+
+  .game > :last-child, p > :last-child {
+    margin-bottom: 0;
   }
 </style>
